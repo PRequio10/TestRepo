3ebb6b5ccbf951d8bdbaf111574421e821159766
John Philip William Requio <requ0005@algonquinlive.com>
2024-06-26 00:19:46 -0400

    1st commit

---
+++ src/main/java/com/mycompany/testgithub/TestGIthub.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testgithub;
+
+
+public class TestGIthub {
+
+    public static void main(String[] args) {
+        
+        // Write your fullname
+        String member1 = "John Requio";
+        String member2;
+        String member3;
+        String member4;
+        
+        System.out.println(member1);
+//        System.out.println(member2);
+//        System.out.println(member3);
+//        System.out.println(member4);
+        
+    }
+}
